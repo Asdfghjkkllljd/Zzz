622a6c8887c5a22713cf18d7f4c19034c021c31a
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-14 14:31:48 +0300

    Update bet_sender.py

--- bet_sender.py
+++ bet_sender.py
@@ -3,8 +3,9 @@ from pyrogram.types import Message
 import asyncio
 from config import CHANNEL_ID
 
-api_id = "9139417"
-api_hash = "d32b6fbbf46b371a8f38925867403394"
+api_id = "20668090"
+api_hash = "19dc666390d73b98aae44b77d4d8d25c"
+"
 
 app = Client('bot', api_id=api_id, api_hash=api_hash)
 
