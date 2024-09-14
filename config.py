bfe862bb5d27e6e90034fc318e50fac422333e39
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-14 14:30:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-TOKEN = "" # Токен бота
-CHANNEL_ID = -100дад # ID канала со ставками
-ADMINS = [б] # ID админов
-BET_URL = "http://t.me/send?start=IV2r4tr4ZuYo" # Ссылка на счет
-CRYPTOPAY_TOKEN = "ка" # Тут CryptoPay токен
+TOKEN = "7405917761:AAEixpXB4VrRPrsS1Dxr70pdFvYEFw_BJkM" # Токен бота
+CHANNEL_ID = -1002129853604 # ID канала со ставками
+ADMINS = [6584286070] # ID админов
+BET_URL = "https://t.me/send?start=IVrAV6WWx3WV" # Ссылка на счет
+CRYPTOPAY_TOKEN = "244808:AAJ8sYMds1BHWPFFX5RhShpATGeM6gw4Dgp" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
-LOGS_ID = -100минус 100 не убирай # ID канала с оплатами
+LOGS_ID = -1002195516136 # ID канала с оплатами
 
-RULES_LINK = "https://t.me/c/2210029843/4" # Ссылка на канал с правилам
-NEWS_LINK = "https://t.me/+FyL7veEf5ugyYjBi" # Ссылка на новостной канал
-OWNER_LINK = "https://t.me/rip_Anarxist" # Ссылка на владельца/поддержку
-PEREHOD_LINK = "@A_X_L_BET" # Ссылка на переходник
-BETS_LINK = "https://t.me/+FY-6NcV50rwxMWQy" # Ссылка на канал ставок
-CASINO_NAME = "A.X.L BET" # Название казино
-BOT_NAME = "A_X_L_robot"
-TEXT = "<b><a href='https://t.me/+FyL7veEf5ugyYjBi'>Новостной канал </a> | <a href='https://t.me/A_X_L_robot'>Реферальный бот</a> | <a href='https://t.me/rip_anarxist'>Техническая поддержка</a></b>"
+RULES_LINK = "https://t.me/igriwa/6" # Ссылка на канал с правилам
+NEWS_LINK = "https://t.me/newscryptogamecc" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/SC999AM" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "@cryptogameccc" # Ссылка на переходник
+BETS_LINK = "https://t.me/cryptogameccc" # Ссылка на канал ставок
+CASINO_NAME = "Crypto Casino" # Название казино
+BOT_NAME = "cryptogameccc_bot"
+TEXT = "<b><a href='https://t.me/newscryptogamecc'>Новостной канал </a> | <a href='https://t.me/cryptogameccc_bot'>Реферальный бот</a> | <a href='https://t.me/SC999AM'>Техническая поддержка</a></b>"
