837de64da81801a8ec19e9a27819cd79f52cbd41
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-14 14:28:07 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = "" # Токен бота
+CHANNEL_ID = -100дад # ID канала со ставками
+ADMINS = [б] # ID админов
+BET_URL = "http://t.me/send?start=IV2r4tr4ZuYo" # Ссылка на счет
+CRYPTOPAY_TOKEN = "ка" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
+LOGS_ID = -100минус 100 не убирай # ID канала с оплатами
+
+RULES_LINK = "https://t.me/c/2210029843/4" # Ссылка на канал с правилам
+NEWS_LINK = "https://t.me/+FyL7veEf5ugyYjBi" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/rip_Anarxist" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "@A_X_L_BET" # Ссылка на переходник
+BETS_LINK = "https://t.me/+FY-6NcV50rwxMWQy" # Ссылка на канал ставок
+CASINO_NAME = "A.X.L BET" # Название казино
+BOT_NAME = "A_X_L_robot"
+TEXT = "<b><a href='https://t.me/+FyL7veEf5ugyYjBi'>Новостной канал </a> | <a href='https://t.me/A_X_L_robot'>Реферальный бот</a> | <a href='https://t.me/rip_anarxist'>Техническая поддержка</a></b>"
